57ce41b2cf32fa5d61add20918c32f08123fb853
harshil sharma <sharmaharshil5212@gmail.com>
2024-01-20 01:52:54 +0530

    Phase1 code Pushed

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,35 +1,22 @@
-
 import { createBrowserRouter } from "react-router-dom";
 
 import AppRouter from "./AppRouter";
 import AuthRouter from "./AuthRouter";
 import HomePage from "../Pages/HomePage/HomePage";
 
-
-
-
 export const ROUTES = {
- 
-  HOME:"/",
- 
-
-
+  HOME: "/",
 };
 
 const navigationRouter = createBrowserRouter([
-
   {
     path: ROUTES.HOME,
     element: (
-      <AuthRouter  >
-      <HomePage />
-    </AuthRouter>
-     
+      <AppRouter>
+        <HomePage />
+      </AppRouter>
     ),
   },
-  
-
- 
 ]);
 
 export default navigationRouter;
--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,8 +1,10 @@
+import Lottie from 'lottie-react'
 import React from 'react'
-
+import contactAnimation from '../../Assets/contactaninmation.json'
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div className=' text-red-600 font-bold w-screen bg-white h-screen flex items-center justify-center overflow-hidden'>
+      <Lottie animationData={contactAnimation }/>     </div>
   )
 }
 
